0dba7aeca95fe4bf13462dcbcc69ff1551db26d2
kadunapikin <ibakoshi27@gmail.com>
2022-11-09 17:44:13 +0100

    add an Id to select element on test.html and a function statment on test.js

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 
 
 const select = document.querySelector ("select");
-const message = document.querySelector ("p");
+const message = document.querySelector ("h1");
+select.addEventListener ("change", weather);
 
 function weather() {
     const choice = select.value;
@@ -11,7 +12,19 @@ function weather() {
     } else {
         message.textContent = "It's winter, no need."
     } 
-}
-select.addEventListener ("change", weather);
+} 
 
-const response = document.querySelector ("h1");
+const chose = document.getElementById ("relate")
+const response = document.querySelector ("h2");
+chose.addEventListener ("change", relationshipStatus);
+
+function relationshipStatus() {
+    const relationship = chose.value;
+    if (relationship === "married") {
+        response.textContent = "You're a responsible person."
+    } else if (relationship === "divorced") {
+        response.textContent = "Na so life be, no be you kill am."
+    } else {
+        response.textContent = "You're not yet responsible my dear!"
+    }
+}
